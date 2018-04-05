28e9961d78d56b91256a7b9bb7a00d968f760c23
Arak00 <Alexandrecartier00@gmail.com>
2018-04-05 23:23:35 +0100

    Push final

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Cache l'élément monBoutton //
 function myFunction() {
   var x = document.getElementById("monBoutton");
   if (x.style.display === "none"){
@@ -6,4 +7,15 @@ function myFunction() {
     x.style.display = "none";
   }
 }
-window.onload=myFunction ;
+// Lance la fonction myFunction au chargement de la page
+window.onload=myFunction();
+// Barre de recherche google personnalisée // 
+(function() {
+  var cx = '008517678358045702984:9tqbuacvf0m'; // Insert your own Custom Search Engine ID here
+  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+  gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
+      '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
+// set up le pop up après 10 secondes d'attente //
+window.onload=setTimeout(function() { alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); },10000);
